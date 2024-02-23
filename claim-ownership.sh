a1056e06b8d2bb5ccad1e66c4f322ec65ad751ce
Cecilia Sanare <ceci@sanare.dev>
2024-02-22 20:02:24 -0600

    chore: tweak ownership file

--- claim-ownership.sh
+++ claim-ownership.sh
@@ -1,3 +1,31 @@
-#!/bin/sh
+#!/bin/bash
 
-sudo chown $USER:$USER -R ./data/pterodactyl/wings/volumes/*
+set -eu
+
+OWNER_UID=$USER
+OWNER_GID=$USER
+
+for ARGUMENT in "$@"
+do
+    case $ARGUMENT in
+        (*:*) OWNER_UID=${ARGUMENT%:*}    OWNER_GID=${ARGUMENT##*:};;
+        (*)   OWNER_UID=$ARGUMENT         OWNER_GID=$ARGUMENT;;
+    esac
+done
+
+
+echo "Preparing to claim ownership for ${OWNER_UID}:${OWNER_GID}..."
+
+read -p "Continue (y/N)? " choice
+
+case "$choice" in 
+  y|Y ) CHOICE="yes";;
+  * ) CHOICE="no";;
+esac
+
+if [[ "$CHOICE" != "yes" ]]; then
+    echo "Canceled"
+    exit 1
+fi
+
+sudo chown $OWNER_UID:$OWNER_GID -R ./data/pterodactyl/wings/volumes/*
